477c8dc532262c34c215a022b8277287e4d76c23
Deshal-001 <kavindu.20200969@iit.ac.lk>
2021-06-25 12:00:45 +0530

    Q5 Done !

--- src/com/company/Tutorials.java
+++ src/com/company/Tutorials.java
@@ -87,25 +87,54 @@ public class Tutorials {
 
 **/
 
-
+/**
      //q4
         int[] tot={1,2,3,4,5};
-        int[] new_Tot=new int[5];
+        int[] new_Tot=new int[tot.length];
         for(int n=0;n<tot.length-1;n++){
             new_Tot[n+1]=tot[n];
             new_Tot[0]=tot[tot.length-1];
-
-
-
-
-
-
 
         }
 
 
 
         System.out.println(Arrays.toString(new_Tot));
+
+ **/
+
+//q5
+
+        double[] num=new double[20];
+
+        for (int i=0;i<20;i++) {
+            Scanner mark = new Scanner(System.in);
+            System.out.print("Enter your Mark:");
+            double marks = mark.nextDouble();
+
+            num[i]=marks;
+
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your i:");
+        int i = input.nextInt();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.print("Enter your j:");
+        int j = input2.nextInt();
+        double x=num[i];
+        double y=num[j];
+
+        num[i]=y;
+        num[j]=x;
+
+        System.out.println(Arrays.toString(num));
+
+
+
+
+
 
 
 
